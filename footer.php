7cebbbda01882dd91a978822a3c721178d4935b8
root@DESKTOP-1OCV68Q <dhordyko@gmail.com>
2022-06-21 11:13:11 +0200

    footer finish

--- footer.php
+++ footer.php
@@ -16,14 +16,13 @@
 		<footer id="footer" class="site-footer pb-3" role="contentinfo">
 		<div class="container ">
 <div class="row">
-<div class="footer-menu-container col-md-3 footer1 p-0"><?php dynamic_sidebar( 'footer-1' ); ?></div>
-<div class="footer-menu-container col-md-3 footer2" ><?php dynamic_sidebar( 'footer-2' ); ?></div>
-<div class="footer-menu-container col-md-3  footer3" ><?php dynamic_sidebar( 'footer-3' ); ?></div>
+<div class="footer-menu-container col-lg-3 footer1 p-0 col-sm-6 col-12"><?php dynamic_sidebar( 'footer-1' ); ?></div>
+<div class="footer-menu-container col-lg-3 col-sm-4 ms-auto ms-lg-0 footer2 col-12" ><?php dynamic_sidebar( 'footer-2' ); ?></div>
+<div class="footer-menu-container col-lg-3 col-sm-6 footer3 col-12" ><?php dynamic_sidebar( 'footer-3' ); ?></div>
 <div class="footer-menu-container col  footer4" ><?php dynamic_sidebar( 'footer-4' ); ?></div>
+<div class="col-md-12 footer5" ><?php dynamic_sidebar( 'footer-5' ); ?></div>
 </div>
-			<div class="row  border-0 p-0" >
-				<div class="col-md-12 footer5" ><?php dynamic_sidebar( 'footer-5' ); ?></div>
-			</div>
+		
 </div>
 
 	</footer><!-- #colophon -->
